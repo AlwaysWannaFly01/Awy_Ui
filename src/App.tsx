ab46aeab6bd65ac27926280d9c4e158794cfbeff
alwayswannafly <15007159069@163.com>
2020-08-31 16:49:23 +0800

    Button组件样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
+import Button, {ButtonSize, ButtonType} from './components/Button/button'
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Hello World</h1>
-                <h2>Hello World</h2>
-                <h3>Hello World</h3>
-                <h4>Hello World</h4>
-                <hr/>
-                <code>
-                    const a = 'b
-                </code>
+                <Button>Hello</Button>
+                <Button disabled>Hello</Button>
+                <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+                <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Hello</Button>
+                <Button btnType={ButtonType.Link} href="http://www.baidu.com">Baidu</Button>
+                <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Baidu disabled</Button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
