44ac459947237c7f8e34ba176444c4f7feb68b0e
alwayswannafly <15007159069@163.com>
2020-09-02 18:07:46 +0800

    Icon图标使用css实现简单旋转效果

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -1,83 +1,101 @@
-import React, {useContext, useState, FunctionComponentElement} from "react";
+import React, { useContext, useState, FunctionComponentElement } from "react";
 import classNames from "classnames";
-import {MenuContext} from "./menu";
-import {MenuItemProps} from './menuItem';
+import { MenuContext } from "./menu";
+import { MenuItemProps } from "./menuItem";
+import Icon from "../Icon/icon";
 
 export interface SubMenuProps {
-    index?: string;
-    title: string;
-    classnName?: string
+  index?: string;
+  title: string;
+  classnName?: string;
 }
 
-const SubMenu: React.FC<SubMenuProps> = ({index, title, children, classnName}) => {
-    const context = useContext(MenuContext)
-    const openedSubmenus = context.defaultOpenSubMenus as Array<string>
-    const isOpened = (index && context.mode === 'vertical') ? openedSubmenus.includes(index) : false
+const SubMenu: React.FC<SubMenuProps> = ({
+  index,
+  title,
+  children,
+  classnName,
+}) => {
+  const context = useContext(MenuContext);
+  const openedSubmenus = context.defaultOpenSubMenus as Array<string>;
+  const isOpened =
+    index && context.mode === "vertical"
+      ? openedSubmenus.includes(index)
+      : false;
+  const [menuOpen, setOpen] = useState(isOpened);
 
-    const classes = classNames('menu-item submenu-item', classnName, {
-        'is-active': context.index === index
-    })
+  const classes = classNames("menu-item submenu-item", classnName, {
+    "is-active": context.index === index,
+    "is-opened": menuOpen,
+    "is-vertical": context.mode === "vertical",
+  });
 
-    const [menuOpen, setOpen] = useState(isOpened);
-    const handleClick = (e: React.MouseEvent) => {
-        e.preventDefault();
-        setOpen(!menuOpen);
-    }
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setOpen(!menuOpen);
+  };
 
-    let timer: any;
-    const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
-        clearTimeout(timer)
-        e.preventDefault()
-        timer = setTimeout(() => {
-            setOpen(toggle)
-        }, 300)
-    }
+  let timer: any;
+  const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
+    clearTimeout(timer);
+    e.preventDefault();
+    timer = setTimeout(() => {
+      setOpen(toggle);
+    }, 300);
+  };
 
-    const clickEvents = context.mode === 'vertical' ? {
-        onClick: handleClick
-    } : {}
-
-    const hoverEvents = context.mode !== 'vertical' ? {
-        onMouseEnter: (e: React.MouseEvent) => {
-            handleMouse(e, true)
-        },
-        onMouseLeave: (e: React.MouseEvent) => {
-            handleMouse(e, false)
+  const clickEvents =
+    context.mode === "vertical"
+      ? {
+          onClick: handleClick,
         }
-    } : {}
+      : {};
 
-    const renderChildren = () => {
-        const subMenuClasses = classNames('awy-submenu', {
-            'menu-opened': menuOpen
-        })
-        const childrenComponent = React.Children.map(children, (child, i) => {
-            const childElement = child as FunctionComponentElement<MenuItemProps>
-            if (childElement.type.displayName === 'MenuItem') {
-                return React.cloneElement(childElement, {
-                    index: `${index}-${i}`
-                })
-            } else {
-                console.error('Warning:SubMenu has a child which is not a MenuItem!')
-            }
-        })
+  const hoverEvents =
+    context.mode !== "vertical"
+      ? {
+          onMouseEnter: (e: React.MouseEvent) => {
+            handleMouse(e, true);
+          },
+          onMouseLeave: (e: React.MouseEvent) => {
+            handleMouse(e, false);
+          },
+        }
+      : {};
 
-        return (
-            <ul className={subMenuClasses} data-testid='vertical-test'>
-                {childrenComponent}
-            </ul>
-        )
-    }
+  const renderChildren = () => {
+    const subMenuClasses = classNames("awy-submenu", {
+      "menu-opened": menuOpen,
+    });
+    const childrenComponent = React.Children.map(children, (child, i) => {
+      const childElement = child as FunctionComponentElement<MenuItemProps>;
+      if (childElement.type.displayName === "MenuItem") {
+        return React.cloneElement(childElement, {
+          index: `${index}-${i}`,
+        });
+      } else {
+        console.error("Warning:SubMenu has a child which is not a MenuItem!");
+      }
+    });
 
     return (
-        <li key={index} className={classes} {...hoverEvents}>
-            <div className="submenu-title" onClick={handleClick} {...clickEvents}>
-                {title}
-            </div>
-            {renderChildren()}
-        </li>
-    )
-}
+      <ul className={subMenuClasses} data-testid="vertical-test">
+        {childrenComponent}
+      </ul>
+    );
+  };
 
-SubMenu.displayName = 'SubMenu'
+  return (
+    <li key={index} className={classes} {...hoverEvents}>
+      <div className="submenu-title" onClick={handleClick} {...clickEvents}>
+        {title}
+        <Icon icon="arrow-alt-circle-down" className="arrow-icon"></Icon>
+      </div>
+      {renderChildren()}
+    </li>
+  );
+};
+
+SubMenu.displayName = "SubMenu";
 
 export default SubMenu;
